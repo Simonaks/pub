831ad4e111e0181c994ca14887e06baf202166dd
Simon Aks <avossesimon@gmail.com>
2025-12-03 23:59:32 +0000

    d'accord

--- add_comments_and_push.ps1
+++ add_comments_and_push.ps1
@@ -3,7 +3,7 @@ for ($i = 1; $i -le 10; $i++) {
   # Make a small modification to the index.html file
   Add-Content -Path 'README.md' -Value "Comment $i`n"
 
-  # Add, commit, and push the changes
+  # Add, commit, and push the  changes
   git add README.md
   git commit -m "Add comment $i"
   git push origin main
